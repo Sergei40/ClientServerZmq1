1658a3158015050dc5dc927ced5667cb42259912
Sergei40 <seregagerasimov@gmail.com>
2021-02-20 21:23:54 +0300

    server sends a data

--- server/server.cpp
+++ server/server.cpp
@@ -4,12 +4,14 @@
 #include <iostream>
 #include <fstream>
 #include <zmq.hpp>
+//#include "zhelpers.hpp"
 #include <bits/stdc++.h>
 
 void readFileInSet(auto& allStudentsSet, std::ifstream& studentFile);
 
 int main(int argc, char* argv[]) 
 {
+    std::unordered_set <std::string> allStudentsSet;
     if (argc < 3) {
         std::cout << "files not found\n";
         return -1;
@@ -18,8 +20,6 @@ int main(int argc, char* argv[])
         std::ifstream studentFile1 (argv[2]);
         std::ifstream studentFile2 (argv[3]);
         if (studentFile1.is_open() && studentFile2.is_open()) {
-            std::unordered_set <std::string> allStudentsSet;
-
             readFileInSet(allStudentsSet, studentFile1);
             readFileInSet(allStudentsSet, studentFile2);
             
@@ -29,31 +29,27 @@ int main(int argc, char* argv[])
             std::cout << "files can't be opened\n";
             return -1;
         }
-    } 
-    using namespace std::chrono_literals;
-
+    }
+     
     // initialize the zmq context with a single IO thread
-    zmq::context_t context{1};
+    zmq::context_t context(1);
 
     // construct a REP (reply) socket and bind to interface
-    zmq::socket_t socket{context, zmq::socket_type::rep};
-    socket.bind("tcp://*:5555");
+    zmq::socket_t publisher(context, ZMQ_PUB);
+    publisher.bind("tcp://*:5555");
 
     // prepare some static data for responses
     const std::string data{"World"};
 
-    for (;;) 
+    while (true) 
     {
-        zmq::message_t request;
-        // receive a request from client
-        socket.recv(request, zmq::recv_flags::none);
-        std::cout << "Received " << request.to_string() << std::endl;
-
+        zmq::message_t message(allStudentsSet.begin(), allStudentsSet.end());
         // simulate work
-        std::this_thread::sleep_for(3s);
+        // snprintf((string)message.data(), 1, 
+        std::this_thread::sleep_for(std::chrono::seconds(3));
 
         // send the reply to the client
-        socket.send(zmq::buffer(data), zmq::send_flags::none);
+        publisher.send(message,zmq::send_flags::dontwait); 
     }
 
     return 0;
