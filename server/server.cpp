7e26c0e6fcff004a42210e32ccb716175342d10e
Sergei40 <61883826+Sergei40@users.noreply.github.com>
2021-03-29 18:00:21 +0300

    Update server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -83,7 +83,6 @@ void readFileInSet(SetOfStrings& allStudentsSet, std::ifstream& studentFile) {
 
 //  Convert string to 0MQ string and send to socket
 static bool s_send (zmq::socket_t & socket, const std::string & string) {
-
     zmq::message_t message(string.size());
     memcpy(message.data(), string.data(), string.size());
 
